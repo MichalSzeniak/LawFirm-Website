adf808295048636a2843534b982967e8622f5c8b
Michał Szeniak <michal.szeniak@gmail.com>
2020-08-21 13:15:15 +0200

    feat: navigation scroll blur

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,17 @@
 import '../scss/main.scss';
 
 
-console.log('HELLO 🚀')
+console.log('HELLO 👋')
+
+const navigation = document.querySelector(".navigation");
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 50) {
+      navigation.classList.add("navigation__scroll")
+    //   logo.classList.add("logo__img--test")
+    }
+    else if (window.pageYOffset < 50) {
+      navigation.classList.remove("navigation__scroll")
+    //   logo.classList.remove("logo__img--test")
+    }
+  })
